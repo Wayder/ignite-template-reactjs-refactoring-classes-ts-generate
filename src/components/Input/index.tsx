348eceed8bcef1a1f142089bc8c9df1f6a658eac
Wayder Loyola <wayderloyola@gmail.com>
2021-09-23 17:53:13 -0300

    :tada: Challenge 02 - Completed

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,22 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
+import { IconType } from 'react-icons';
+
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+  icon?: IconType
+}
+
+export const Input = ({ name, icon: Icon, ...rest }: Props) => {
+  const inputRef = useRef(null) as any;
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -50,4 +58,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   );
 };
 
-export default Input;
